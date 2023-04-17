876d4079a553d9bf3314c912d624b3974634aa1b
Jaan Altosaar <jaan@onefact.org>
2023-04-17 19:25:32 -0400

    update with @mictadiello

--- data/cms_hospital_info/load.sql
+++ data/cms_hospital_info/load.sql
@@ -1,1 +1,2 @@
-COPY cms_hospital_info FROM 'cms_hospital_info/cms_hospital_info.parquet' (FORMAT 'parquet');
+COPY cms_hospital_info FROM './data/cms_hospital_info/cms_hospital_info.parquet' (FORMAT 'parquet');
+COPY gazzetteer FROM './data/cms_hospital_info/gazzetteer.parquet' (FORMAT 'parquet');
