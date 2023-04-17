6a4b6ce7ee31bf00ab5c6d10237483a58d739e8f
Jaan Altosaar <jaan@onefact.org>
2023-04-17 14:12:19 -0400

    add map of all hospitals in altair

---
+++ data/cms_hospital_info/load.sql
@@ -0,0 +1,1 @@
+COPY cms_hospital_info FROM 'cms_hospital_info/cms_hospital_info.parquet' (FORMAT 'parquet');
---
+++ data/gazzetteer/load.sql
@@ -0,0 +1,2 @@
+COPY gazzetteer FROM 'gazzetteer/gazzetteer.parquet' (FORMAT 'parquet');
+COPY cms_hospital_info FROM 'gazzetteer/cms_hospital_info.parquet' (FORMAT 'parquet');
